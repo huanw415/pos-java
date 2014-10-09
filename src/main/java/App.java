fa86df32e8ca45bcfec9ca10cb78f5259bdc7445
zzh <xiaoyanzhuzzh@sina.com>
2014-10-09 20:40:25 +0800

    read cart.txt

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class App {
@@ -6,6 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         initItems();
+        ArrayList<String> cartInfo = readCartText();
     }
     static void initItems(){
         Item item1 = new Item("ITEM00001", "橙汁", "瓶", 5.00);
@@ -22,5 +27,24 @@ public class App {
 //        for(Item item: items){
 //            System.out.println(item.name);
 //        }
+    }
+
+    static ArrayList<String> readCartText(){
+        ArrayList<String> cartInfo = new ArrayList<String>();
+        try {
+            File cartLocation = new File("/home/zhangzhihui/pos-java/src/main/resources/cart.txt");
+            FileReader cartReader = new FileReader(cartLocation);
+            BufferedReader reader = new BufferedReader(cartReader);
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                cartInfo.add(line);
+            }
+//            System.out.println(cartInfo);
+            reader.close();
+        } catch (IOException ex) {
+            System.out.println("fail read file!");
+        }
+        return cartInfo;
     }
 }
