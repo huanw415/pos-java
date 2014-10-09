cf64c5a8ca2bc38cdfe5b98cfb064ecb40953ba6
zzh <xiaoyanzhuzzh@sina.com>
2014-10-09 20:15:30 +0800

    new five item object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) {
         //TODO: Need to implement.
-        Item item = new Item("ITEM00001", "橙汁", "瓶", 5.00);
+        Item item1 = new Item("ITEM00001", "橙汁", "瓶", 5.00);
+        Item item2 = new Item("ITEM00002", "可乐", "瓶", 3.00);
+        Item item3 = new Item("ITEM00003", "苹果", "斤", 4.50);
+        Item item4 = new Item("ITEM00004", "雪碧", "瓶", 5.00);
+        Item item5 = new Item("ITEM00005", "方便面", "袋", 2.50);
     }
 }
