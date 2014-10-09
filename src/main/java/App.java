d29bd78ac6a84123c2403dd3b1f05ae5429fdba3
zzh <xiaoyanzhuzzh@sina.com>
2014-10-09 19:37:38 +0800

    read buy_two_get_one_free_promotion file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         //TODO: Need to implement.
-
+        Item item = new Item("ITEM00001", "橙汁", "瓶", 5.00);
+        item.getPromotion();
     }
 }
--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -7,19 +7,19 @@ public class Item {
     public String name;
     public String unit;
     public double price;
-    public ArrayList<String> promotion;
-    private ArrayList<String> promotionItems;
+    public ArrayList<String> promotion = new ArrayList<String>();
+    private ArrayList<String> promotionItems = new ArrayList<String>();
 
-    item(String barcode, String name, String unit, String price){
-        barcode = barcode;
-        name = name;
-        unit = unit;
-        price = price;
+    Item(String barcode, String name, String unit, double price){
+        this.barcode = barcode;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
     }
     public void getPromotion(){
 
         try{
-            File promotion1 = new File("buy_two_get_one_free_promotion.txt");
+            File promotion1 = new File("/home/zhangzhihui/pos-java/src/main/resources/buy_two_get_one_free_promotion.txt");
             FileReader promotion1Reader = new FileReader(promotion1);
             BufferedReader reader1 = new BufferedReader(promotion1Reader);
 
@@ -31,6 +31,7 @@ public class Item {
             if(isExist){
                 promotion.add("buy_two_get_one_free_promotion");
             }
+            System.out.println(promotion);
         }catch(IOException ex){
             System.out.println("fail read file!");
         }
