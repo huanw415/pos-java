ef1b525726270beb02b581b8da73805df3a2ffca
zzh <xiaoyanzhuzzh@sina.com>
2014-10-09 20:05:22 +0800

    add getOnePromotion function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) {
         //TODO: Need to implement.
         Item item = new Item("ITEM00001", "橙汁", "瓶", 5.00);
-        item.getPromotion();
+        item.getPromotions();
     }
 }
--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -8,7 +8,7 @@ public class Item {
     public String unit;
     public double price;
     public ArrayList<String> promotion = new ArrayList<String>();
-    private ArrayList<String> promotionItems = new ArrayList<String>();
+    private ArrayList<String> promotionText = new ArrayList<String>();
 
     Item(String barcode, String name, String unit, double price){
         this.barcode = barcode;
@@ -16,27 +16,42 @@ public class Item {
         this.unit = unit;
         this.price = price;
     }
-    public void getPromotion(){
+    
+    public void getPromotions() {
+        ArrayList<String> promotionLocations = new ArrayList<String>();
+        promotionLocations.add("/home/zhangzhihui/pos-java/src/main/resources/buy_two_get_one_free_promotion.txt");
+        promotionLocations.add("/home/zhangzhihui/pos-java/src/main/resources/second_half_price_promotion.txt");
+        promotionLocations.add("/home/zhangzhihui/pos-java/src/main/resources/discount_promotion.txt");
 
-        try{
-            File promotion1 = new File("/home/zhangzhihui/pos-java/src/main/resources/buy_two_get_one_free_promotion.txt");
+        for (int i = 0; i < promotionLocations.size(); i++) {
+            getOnePromotion(promotionLocations, i);
+        }
+        System.out.println(promotion);
+    }
+    
+    private void getOnePromotion(ArrayList<String> promotionLocations, int i) {
+
+        ArrayList<String> promotionNames = new ArrayList<String>();
+        promotionNames.add("buy_two_get_one_free_promotion");
+        promotionNames.add("second_half_price_promotion");
+        promotionNames.add("discount_promotion");
+
+        try {
+            File promotion1 = new File(promotionLocations.get(i));
             FileReader promotion1Reader = new FileReader(promotion1);
             BufferedReader reader1 = new BufferedReader(promotion1Reader);
 
             String line = null;
-            while((line = reader1.readLine()) != null){
-                promotionItems.add(line);
+            while ((line = reader1.readLine()) != null) {
+                promotionText.add(line);
             }
-            boolean isExist = promotionItems.contains(barcode);
-            if(isExist){
-                promotion.add("buy_two_get_one_free_promotion");
+            
+            boolean isExist = promotionText.contains(barcode);
+            if (isExist) {
+                promotion.add(promotionNames.get(i));
             }
-            System.out.println(promotion);
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println("fail read file!");
         }
-
-    }
-
-
+    }   
 }
