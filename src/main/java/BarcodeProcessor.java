5fb508a993760b265006cdd13544e00f0598e354
zzh <xiaoyanzhuzzh@sina.com>
2014-10-10 19:05:24 +0800

    rename barcode to Barcode

---
+++ src/main/java/BarcodeProcessor.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class BarcodeProcessor {
+
+    public ArrayList<String> barcodes = new ArrayList<String>();
+
+    public BarcodeProcessor(ArrayList<String> inputs){
+        this.getBarcodes(inputs);
+    }
+
+    private void getBarcodes(ArrayList<String> inputs) {
+        for(int i = 0; i < inputs.size(); i++) {
+
+            String[] barcodeAndNumber = inputs.get(i).split("-");
+            String barcode = barcodeAndNumber[0];
+            double number;
+            if (barcodeAndNumber.length == 1) {
+                number = 1;
+            } else {
+                number = Double.parseDouble(barcodeAndNumber[1]);
+            }
+
+            int count = (int) number;
+            for(int j = 0; j < count; j++){
+                barcodes.add(barcode);
+//                System.out.println(barcodes);
+            }
+        }
+    }
+
+}
+
--- src/main/java/barcodeProcessor.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by zhangzhihui on 14-10-10.
- */
-public class barcodeProcessor {
-}
