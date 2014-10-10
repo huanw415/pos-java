eba651c3e80ab58ebfafb570840efa9ebe16df1f
zzh <xiaoyanzhuzzh@sina.com>
2014-10-10 22:40:21 +0800

    add printAllCartItems

--- src/main/java/com/thoughtworks/iamcoach/pos/Printer.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Printer.java
@@ -1,4 +1,25 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.util.ArrayList;
+
 public class Printer {
+    public ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
+
+    public Printer(ArrayList<CartItem> cartItems){
+        this.cartItems = cartItems;
+    }
+
+    public void printAllCartItems(){
+
+        for(int i=0; i<cartItems.size(); i++){
+            System.out.println("名称：" + cartItems.get(i).name + "   数量：" + cartItems.get(i).num
+                    + "   单价：" + cartItems.get(i).price + "   单位：" + cartItems.get(i).unit
+                    + "   小计：" + cartItems.get(i).getSubTotal()
+            );
+
+        }
+
+    }
+
+
 }
