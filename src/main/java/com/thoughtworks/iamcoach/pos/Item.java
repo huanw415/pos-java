8d5281d71bfdbc5116e4869c4fff7beaba0c9635
zzh <xiaoyanzhuzzh@sina.com>
2014-10-11 20:52:54 +0800

    add setter and getter

--- src/main/java/com/thoughtworks/iamcoach/pos/Item.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Item.java
@@ -6,21 +6,58 @@ import java.util.ArrayList;
 
 public class Item {
 
-    public String barcode;
-    public String name;
-    public String unit;
-    public double price;
-    public ArrayList<String> promotion = new ArrayList<String>();
+    private String barcode;
+    private String name;
+    private String unit;
+    private double price;
+    private ArrayList<String> promotion = new ArrayList<String>();
     private ArrayList<String> promotionText = new ArrayList<String>();
 
+
     public Item(String barcode, String name, String unit, double price){
-        this.barcode = barcode;
-        this.name = name;
-        this.unit = unit;
-        this.price = price;
+//        this.barcode = barcode;
+//        this.name = name;
+//        this.unit = unit;
+//        this.price = price;
+        this.setBarcode(barcode);
+        this.setName(name);
+        this.setUnit(unit);
+        this.setPrice(price);
         this.getPromotions();
     }
-    
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getBarcode(){
+        return this.barcode;
+    }
+    public void setBarcode(String barcode){
+        this.barcode = barcode;
+    }
+
+    public String getUnit(){
+        return this.unit;
+    }
+    public void setUnit(String unit){
+        this.unit = unit;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public ArrayList<String> getPromotion(){
+        return this.promotion;
+    }
+
     public void getPromotions() {
 
         ArrayList<Path> promotionLocations = new ArrayList<Path>();
