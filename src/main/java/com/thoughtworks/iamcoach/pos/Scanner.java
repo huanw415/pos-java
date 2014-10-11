6ab1e2caf212b7b50a65a82cc226c2cdde1f8db4
zzh <xiaoyanzhuzzh@sina.com>
2014-10-11 21:20:48 +0800

    delete useless code

--- src/main/java/com/thoughtworks/iamcoach/pos/Scanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Scanner.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 
 public class Scanner {
 
-//    private ArrayList<String> inputs = getInputs();
-
     public ArrayList<String> getInputs(){
         ArrayList<String> inputs = new ArrayList<String>();
 
