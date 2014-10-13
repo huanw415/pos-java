fa2450fd4d10725966fd35ab765b6b5563f7e9cb
zzh <xiaoyanzhuzzh@sina.com>
2014-10-13 21:11:44 +0800

    add can_caLculate_second_half_price_promotion test

--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
@@ -20,4 +20,17 @@ public class PromotionCalculatorTest {
         assertThat(promotionCalculator.caLculateBuyTwoGetOneFreePromotion(cartItem)).isEqualTo(20.0);
     }
 
+    @Test
+    public void can_caLculate_second_half_price_promotion(){
+        CartItem cartItem = mock(CartItem.class);
+        when(cartItem.getName()).thenReturn("橙汁");
+        when(cartItem.getBarcode()).thenReturn("ITEM000001");
+        when(cartItem.getPrice()).thenReturn(5.00);
+        when(cartItem.getUnit()).thenReturn("瓶");
+        when(cartItem.getNum()).thenReturn(5.0);
+
+        PromotionCalculator promotionCalculator = new PromotionCalculator();
+        assertThat(promotionCalculator.caLculateSecondHalfPricePromotion(cartItem)).isEqualTo(20.00);
+    }
+
 }
