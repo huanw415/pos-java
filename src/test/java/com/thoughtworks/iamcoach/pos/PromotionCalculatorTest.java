87ce3a73a870744bf5b3537a6a59f4b315c2f41a
zzh <xiaoyanzhuzzh@sina.com>
2014-10-13 21:12:54 +0800

    add can_calculator_discount_promotion test

--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
@@ -32,5 +32,16 @@ public class PromotionCalculatorTest {
         PromotionCalculator promotionCalculator = new PromotionCalculator();
         assertThat(promotionCalculator.caLculateSecondHalfPricePromotion(cartItem)).isEqualTo(20.00);
     }
+    @Test
+    public void can_caLculate_discount_promotion(){
+        CartItem cartItem = mock(CartItem.class);
+        when(cartItem.getName()).thenReturn("橙汁");
+        when(cartItem.getBarcode()).thenReturn("ITEM000001");
+        when(cartItem.getPrice()).thenReturn(5.00);
+        when(cartItem.getUnit()).thenReturn("瓶");
+        when(cartItem.getNum()).thenReturn(5.0);
 
+        PromotionCalculator promotionCalculator = new PromotionCalculator();
+        assertThat(promotionCalculator.caLculateDiscountPromotion(cartItem)).isEqualTo(18.75);
+    }
 }
