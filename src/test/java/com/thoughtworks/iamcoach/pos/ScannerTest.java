6bbbddf6715514e7d63c386b531d92d512228234
zzh <xiaoyanzhuzzh@sina.com>
2014-10-13 21:23:15 +0800

    add can_getInputs test

--- src/test/java/com/thoughtworks/iamcoach/pos/ScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ScannerTest.java
@@ -1,7 +1,15 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by zhangzhihui on 14-10-13.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ScannerTest {
+    @Test
+    public void can_getInputs(){
+        Scanner scanner = new Scanner();
+
+        assertThat(scanner.getInputs().size()).isEqualTo(9);
+        assertThat(scanner.getInputs().get(0)).isEqualTo("ITEM000001");
+    }
 }
