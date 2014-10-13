14361aef4333cb88448c406e4b866aba2715f690
zzh <xiaoyanzhuzzh@sina.com>
2014-10-13 21:06:33 +0800

    add can_caLculate_buy_two_get_one_free_promotion test

--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by zhangzhihui on 14-10-13.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class PromotionCalculatorTest {
+    @Test
+    public void can_caLculate_buy_two_get_one_free_promotion(){
+        CartItem cartItem = mock(CartItem.class);
+        when(cartItem.getName()).thenReturn("橙汁");
+        when(cartItem.getBarcode()).thenReturn("ITEM000001");
+        when(cartItem.getPrice()).thenReturn(5.00);
+        when(cartItem.getUnit()).thenReturn("瓶");
+        when(cartItem.getNum()).thenReturn(5.0);
+
+        PromotionCalculator promotionCalculator = new PromotionCalculator();
+        assertThat(promotionCalculator.caLculateBuyTwoGetOneFreePromotion(cartItem)).isEqualTo(20.0);
+    }
+
 }
