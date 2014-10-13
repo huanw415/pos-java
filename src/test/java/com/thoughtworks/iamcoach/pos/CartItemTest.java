282de583d38798e2d0b4970fe75bc96ac7ea8b2f
zzh <xiaoyanzhuzzh@sina.com>
2014-10-13 20:56:13 +0800

    add can_get_subTotal test

--- src/test/java/com/thoughtworks/iamcoach/pos/CartItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/CartItemTest.java
@@ -26,4 +26,24 @@ public class CartItemTest {
         assertThat(cartItem.getPrice()).isEqualTo(5.00);
         assertThat(cartItem.getPromotion().get(0)).isEqualTo("buy_two_get_one_free_promotion");
     }
+
+    @Test
+    public void can_get_subTotal() {
+        Item item = mock(Item.class);
+        when(item.getName()).thenReturn("橙汁");
+        when(item.getBarcode()).thenReturn("ITEM000001");
+        when(item.getPrice()).thenReturn(5.00);
+        when(item.getUnit()).thenReturn("瓶");
+
+        double number = 4.00;
+        CartItem cartItem = new CartItem(item, number);
+
+        PromotionCalculator promotionCalculator = mock(PromotionCalculator.class);
+        when(promotionCalculator.caLculateBuyTwoGetOneFreePromotion(cartItem)).thenReturn(20.00);
+        when(promotionCalculator.caLculateSecondHalfPricePromotion(cartItem)).thenReturn(15.00);
+        when(promotionCalculator.caLculateDiscountPromotion(cartItem)).thenReturn(23.00);
+
+        assertThat(cartItem.getSubTotal()).isEqualTo(15.00);
+    }
+
 }
