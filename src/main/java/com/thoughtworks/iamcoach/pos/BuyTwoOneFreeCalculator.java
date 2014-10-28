d992e23022b475bb43afc99da9a05f3778fe4bf0
adabeauty <654296217@qq.com>
2014-10-28 18:48:12 +0800

    rename BuyTwoOneFreePromotion to BuyTwoOneFreeCalculator

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/BuyTwoOneFreeCalculator.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.iamcoach.pos;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wanghuan on 14-10-28.
+ */
+public class BuyTwoOneFreeCalculator extends PromotionCalculator{
+
+}
--- src/main/java/com/thoughtworks/iamcoach/pos/BuyTwoOneFreePromotion.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.iamcoach.pos;
-
-/**
- * Created by wanghuan on 14-10-28.
- */
-public class BuyTwoOneFreePromotion {
-}
