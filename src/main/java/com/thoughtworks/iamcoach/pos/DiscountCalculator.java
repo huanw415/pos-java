6914ed2e39090c3a3347af9daf3fbc1ff3099a6c
adabeauty <654296217@qq.com>
2014-10-28 19:13:09 +0800

    move calculateDiscount to DiscountPromotion

--- src/main/java/com/thoughtworks/iamcoach/pos/DiscountCalculator.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/DiscountCalculator.java
@@ -1,7 +1,39 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by wanghuan on 14-10-28.
- */
-public class DiscountCalculator {
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+
+public class DiscountCalculator extends PromotionCalculator{
+    @Override
+    public double calculatePromotion(CartItem cartItem) {
+        double discount = getDiscount(cartItem);
+        return cartItem.getNum() * discount * cartItem.getPrice();
+    }
+
+    private double getDiscount(CartItem cartItem){
+        double discount = 0;
+        ArrayList<String> discountPromotions = getDiscountPromotion();
+
+        for (String aDiscountPromotion: discountPromotions){
+            String[] currentPromotion = aDiscountPromotion.split(":");
+            if(cartItem.getBarcode().equals(currentPromotion[0])){
+                discount = Double.parseDouble(currentPromotion[1])/100;
+            }
+        }
+        return discount;
+    }
+
+
+    private ArrayList<String> getDiscountPromotion(){
+
+        ArrayList<String> promotionText = new ArrayList<String>();
+        try {
+            Path path = FileSystems.getDefault().getPath("src/main/resources/", "discount_promotion.txt");
+            promotionText = (ArrayList<String>) Files.readAllLines(path);
+        } catch (IOException ex) {
+            System.out.println("fail read file!");
+        }
+        return promotionText;
+    }
 }
--- src/main/java/com/thoughtworks/iamcoach/pos/PromotionCalculator.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/PromotionCalculator.java
@@ -10,33 +10,4 @@ public abstract class PromotionCalculator {
 
     public abstract double calculatePromotion(CartItem cartItem);
 
-    public double caLculateDiscountPromotion(CartItem cartItem){
-        double discount = getDiscount(cartItem);
-        return cartItem.getNum() * discount * cartItem.getPrice();
-    }
-
-    private double getDiscount(CartItem cartItem){
-        double discount = 0;
-        ArrayList<String> discountPromotions = getDiscountPromotion();
-
-        for (String aDiscountPromotion: discountPromotions){
-            String[] currentPromotion = aDiscountPromotion.split(":");
-            if(cartItem.getBarcode().equals(currentPromotion[0])){
-                discount = Double.parseDouble(currentPromotion[1])/100;
-            }
-        }
-        return discount;
-    }
-
-    private ArrayList<String> getDiscountPromotion(){
-
-        ArrayList<String> promotionText = new ArrayList<String>();
-        try {
-            Path path = FileSystems.getDefault().getPath("src/main/resources/", "discount_promotion.txt");
-            promotionText = (ArrayList<String>) Files.readAllLines(path);
-        } catch (IOException ex) {
-            System.out.println("fail read file!");
-        }
-        return promotionText;
-    }
 }
